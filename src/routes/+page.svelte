30e14fc6b15b198db9e061d13791edcdcf59e1f8
samar018 <45222438+samar018@users.noreply.github.com>
2023-08-15 04:04:09 +0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<div class="top-section">
+	<h1><strong>Md Shamim Ahmed </strong></h1>
+	<h2><strong>ID: 2263466 </strong></h2>
+	<h3><strong>University Name: Hasselt University</strong></h3>
+</div>
+
+<style>
+	/* General styles */
+
+	/* Top section styles */
+	.top-section {
+		text-align: center;
+		padding: 4rem 0;
+		background-color: #f0f0f0;
+	}
+
+	.top-section h1 {
+		margin: 0;
+		font-size: 1.5rem;
+	}
+
+	.top-section h2 {
+		margin: 0;
+		font-size: 1rem;
+		font-weight: bold;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,98 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+
+
+
+<header>
+	<div class="top-section">
+		<h1>Md Shamim Ahmed	</h1>
+		<h2>ID - 2263466  </h2>
+		<h3>Hasselt University </h3>
+	</div>
+	
+	
+<nav>
+  <ul>
+    <li aria-current={$page.url.pathname === '/visual' ? 'page' : undefined}>
+      <a href="/visual" target="_self">HOME</a>
+    </li>
+    <li aria-current={$page.url.pathname === '/GeneData' ? 'page' : undefined}>
+      <a href="/GeneData" target="_self">Gene Overview</a>
+    </li>
+    <li aria-current={$page.url.pathname === '/interactions' ? 'page' : undefined}>
+      <a href="/interactions" target="_self">Interactions Overview</a>
+    </li>
+  </ul>
+</nav>
+
+
+</header>
+
+<style>
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: var(--color-theme-1); /* Define your theme color */
+		padding: 1rem;
+		color: white;
+	}
+
+	.top-section {
+		text-align: center;
+		margin-bottom: 1rem;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		background-color: var(--background);
+		padding: 0.5rem;
+	}
+
+	/* svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	} */
+
+	/* path {
+		fill: var(--background);
+	} */
+
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+	}
+
+	li {
+		margin: 0 0.5rem;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid white;
+	}
+
+	nav a {
+		color: white;
+		text-decoration: none;
+		font-weight: 600;
+		transition: color 0.2s ease-in-out;
+	}
+
+	a:hover {
+		color: var(--color-theme-2); /* Define another color for hover */
+	}
+</style>
